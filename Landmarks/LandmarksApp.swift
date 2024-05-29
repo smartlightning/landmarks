ab03e953b5afe7710cbbca2a0b809a7d96009291
chrismcqueen <chris.mcqueen@bergfreunde.de>
2024-05-29 07:43:04 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Chris McQueen on 29.05.24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
