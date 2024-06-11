d3101d51692d88ec67db7153f6502eab97a55242
chrismcqueen <chris.mcqueen@bergfreunde.de>
2024-06-11 15:04:08 +0200

    Fix landmarks list

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -4,10 +4,22 @@ import SwiftUI
 struct LandmarkList: View {
     // Body of the landmark list view
     var body: some View {
-        // List view containing all landmarks
-        List(landmarks) { landmark in
-            // Display each landmark row
-            LandmarkRow(landmark: landmark)
+        // Split view for navigation
+        NavigationSplitView {
+            // List view containing all landmarks
+            List(landmarks) { landmark in
+                // Navigation link to landmark details
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    // Row view for each landmark
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks") // Set navigation title
+        } detail: {
+            // Default text when no landmark is selected
+            Text("Select a Landmark")
         }
     }
 }
